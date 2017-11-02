856a710911b9de2e3b1c73f9e7c068baa1a77967
Dale Biggs <dale.biggs@mymona.uwi.edu>
2017-11-02 08:21:37 +0000

    Complete Exercise 4

--- request.php
+++ request.php
@@ -12,7 +12,61 @@ $definition = [
     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+    
 ];
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+
+    <ol>
+        <li>
+            <h3> Definition</h3>
+            <p>A statement of the exact meaning of a word, especially in a dictionary.</p>
+            <p>-Kimberly</p>
+        </li>
+        
+        <li> 
+            <h3>bar</h3> 
+            <p>A place that sells alcholic beverages</p>
+            <p>-Dale</p>
+        </li>
+            
+        <li> 
+            <h3>ajax</h3> 
+            <p>Technique which involves the use of javascript and xml (or JSON)</p>
+            <p>-Paul</p>
+        </li>
+            
+        <li> 
+            <h3>html</h3> 
+            <p>The standard markup language for creating web pages and web applications.</p>
+            <p>-Alec</p>
+        </li>
+             
+        <li>
+            <h3>css</h3> 
+            <p>A style sheet language used for describing the presentation of a document written in a markup language.</p>
+            <p>-Orville</p>
+        </li>
+            
+        <li> 
+            <h3>javascript</h3> 
+            <p>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</p>
+            <p>-Shea</p>
+        </li>
+            
+        <li> 
+            <h3>php</h3> 
+            <p>A server-side scripting language, and a powerful tool for making dynamic and interactive websites</p>
+            <p>-Anthony</p>
+        </li>
+    
+    </ol>';
+
+header('Content-Type: text/xml');
+
+$xmlOutput = new SimpleXMLElement($xmldata);
+
+echo $xmlOutput->asXML();
--- webdict.js
+++ webdict.js
@@ -4,8 +4,10 @@ window.onload=function(){
     var search=document.getElementById("search");
     var input=document.getElementById("input");
     var result=document.getElementById("result");
+    var getall=document.getElementById("getall");
     
-    search.onclick=function(){
+    
+    search.addEventListener("click", function(){
         var httpRequest= new XMLHttpRequest();
         var url= "https://info2180-lab6-dalebiggs.c9users.io/request.php?q=".concat(input.value);
         
@@ -19,9 +21,38 @@ window.onload=function(){
                         var response=httpRequest.responseText;
                         result.innerHTML=response;
                     }else{
-                        alert("recheck code");
+                        alert("Check input");
                     }
                 }
             }
+    });
+    
+    getall.addEventListener("click", function(){
+        var xhttp = new XMLHttpRequest();
+        var url2="https://info2180-lab6-dalebiggs.c9users.io/request.php?q=&all=true ";
+        
+        xhttp.onreadystatechange = handler2;
+        xhttp.open("GET", url2);
+        xhttp.send();
+        
+        function handler2(){
+            var response2=document.createElement("ol");
+            if (xhttp.readyState == XMLHttpRequest.DONE){
+                
+                if(xhttp.status == 200) {
+                    response2 = xhttp.responseText;
+                    result.innerHTML=response2;
+            }else{
+                alert("Check input");
+            }
+        }
+
+
     }
-}
+    
+    
+});
+
+
+
+};
